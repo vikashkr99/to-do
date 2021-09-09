13a70ebf5f86b6326e1e20995668824f8d273e12
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edu.in>
2021-09-09 19:32:28 +0530

    local  storage added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 function loaded() {
 
+    let key = window.localStorage.getItem('to-do');
+    if (key === null) {
+        alert('Welcome New User');
+        window.localStorage.setItem('to-do', '[]');
+    }
 
+    else {
+        var stored_data = JSON.parse(localStorage.getItem('to-do'));
+        for (i in stored_data) {
+            let li = document.createElement("li");
+            let textnode = document.createTextNode(stored_data[i]);
+            li.appendChild(textnode);
+            document.getElementById('view_list').appendChild(li);
+        }
+    }
 
     document.getElementById('add_command').addEventListener('click', function () {
         
@@ -11,11 +25,13 @@ function loaded() {
         }
 
         else {
+            stored_data.push(userText);
             let li = document.createElement("li");
             let textnode = document.createTextNode(userText);
             li.appendChild(textnode);
             document.getElementById('view_list').appendChild(li);
             document.getElementById('userInput').value = "";
+            window.localStorage.setItem('to-do', JSON.stringify(stored_data));
         }
 
     });
